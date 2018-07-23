29d35ad56c456c7be8c2fb6c8b73b09422c64395
wyattcol <35353860+wyattcol@users.noreply.github.com>
2018-07-23 16:36:53 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -22,9 +22,38 @@ client.on('message', message => {
   // If the message is "ping"
   if (message.content === '!status') {
     // Send "pong" to the same channel
-    message.channel.send('In Development. Coming soon!');
+   message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
   }
 });
+});
+});
+
 
 // Log our bot in
 client.login(token);

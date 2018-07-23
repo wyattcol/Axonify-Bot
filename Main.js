a1af5969ec5afc54962dfc09c89e064bcd18f282
wyattcol <35353860+wyattcol@users.noreply.github.com>
2018-07-23 18:18:23 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -22,10 +22,10 @@ client.on('message', message => {
   // If the message is "ping"
   if (message.content === '!status') {
     // Send "pong" to the same channel
-      const numEmb = new Discord.RichEmbed()
-  .setColor(0xFFFF00)
-  .setAuthor("basic", 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Yes_Check_Circle.svg/120px-Yes_Check_Circle.svg.png')
-  .addField("Axonify is currently in development! Releasing soon.")
+  message.channel.send({embed: {
+      color: 3447003,
+  description: "Axonify is currently in development! Releasing soon."
+}});
   // message.channel.send("`Axonify is currently in development! Releasing soon.`")
   }
 });

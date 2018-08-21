7cf841888b7b3396dec6d9173dfb3ae96941ef70
wyattcol <35353860+wyattcol@users.noreply.github.com>
2018-08-21 15:45:10 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -9,7 +9,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 // The token of your bot - https://discordapp.com/developers/applications/me
-const token = 'NDcxMDQyOTEyMTk3NTQxODg5.Djumog.nXFl1mL5d-x1fGCEPKH0sjfBrIk';
+const token = 'NDgwNDgxMTU0MjQ4NDc0NjQ1.Dl37og.1G2R9DJyfXBTs_uXVPvFx2az4PQ';
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
@@ -17,28 +17,41 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
-// Create an event listener for messages
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-var itemwon = "";
+
+client.on("ready", () => {
+    client.channels.get("427205992015462401").send({embed: {
+        color: 3066993,
+        author: {
+          name: client.user.username,
+          icon_url: client.user.avatarURL
+        },
+        title: "The Bot has Updated!",
+        url: "https://discord.gg/6C3xjY4",
+        description: "Riverrain123 Bot has Updated!",
+        fields: [{
+            name: "Started Development!",
+            value: "The Riverrain123 bot is now in development. Hoping for release later this week."
+          }
+        ],
+        timestamp: new Date(),
+        footer: {
+          icon_url: client.user.avatarURL,
+          text: "Bot developed by Orangedude4221"
+        }
+      }
+    });
+});
+
+
+
 client.on('message', message => {
   // If the message is "ping"
-  if (message.content === '!SpinBasicCrate') {
-    // Send "pong" to the same channel
-    
-    var int = getRandomInt(2);
-    if (int = 1) {
-    itemwon = "1$";
-    }
-    else if (int = 2) {
-    itemwon = "2$";
-    }
+  var words = message.content.split(' ')
+	if (words[0] === '!!vote') {
   message.channel.send({embed: {
       color: 3447003,
-  description: "You recieved a " + itemwon
+  description: "Successfully Voted for the " + words[1] + " Series!"
 }});
-  // message.channel.send("`Axonify is currently in development! Releasing soon.`")
   }
 });
 
